0bd9b2c41cf18a4d668a9295e057386d82e3eeff
Andrew Mayorov <encube.ul@gmail.com>
2022-05-31 21:28:56 +0300

    Drop card validation (#3)
    In favor of valitydev/bankcard-validator.

--- apps/cds/src/cds_thrift_handler_utils.erl
+++ apps/cds/src/cds_thrift_handler_utils.erl
@@ -4,8 +4,6 @@
 -export([raise/1]).
 -export([raise_keyring_unavailable/0]).
 
--export([map_validation_error/1]).
-
 %%
 %% API
 %%
@@ -33,22 +31,9 @@ raise_keyring_unavailable() ->
 raise(Exception) ->
     woody_error:raise(business, Exception).
 
--spec map_validation_error(cds_card_data:reason()) -> ErrorMessage :: binary().
-map_validation_error(unrecognized) ->
-    <<"Unrecognized payment system">>;
-map_validation_error({invalid, Field, Check}) ->
-    Formatted = io_lib:format("Invalid ~p: ~p", [
-        Field,
-        map_validation_check(Check)
-    ]),
-    list_to_binary(Formatted).
-
 %%
 %% Internals
 %%
-
-map_validation_check({length, _}) -> length;
-map_validation_check(Check) -> Check.
 
 % Known safe errors
 filter_error_reason({hash_collision_detected, _Hash} = Reason) ->
